2eb2693e66d66b21e3700c45eb8b66277a895651
Alok Kumar <75585339+Alok-debug@users.noreply.github.com>
2022-05-12 21:51:29 +0530

    add: css property
    added paragraph in liveSpace

--- new_assignment/src/LiveSpace.js
+++ new_assignment/src/LiveSpace.js
@@ -5,6 +5,7 @@ function LiveSpace() {
     return (
     <div className='liveSpace__container'>
         <h1>Live Space</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
     </div>
     
     
